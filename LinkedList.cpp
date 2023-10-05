dde5f1908459f6d7e5533cea674802ab3d56b79a
MatthewStuckenbruck <143656138+MatthewStuckenbruck@users.noreply.github.com>
2023-10-04 21:56:50 -0600

    Add files via upload

---
+++ LinkedList.cpp
@@ -0,0 +1,107 @@
+#include "LinkedList.hpp"
+
+using namespace std;
+
+// TODO
+// Destructor
+LinkedList::~LinkedList()
+{
+   //TODO
+   cout<<"network deleted"<<endl;
+}
+
+// TODO
+// Find a specific key and return a pointer to that node, else return NULL
+// Given the key this function will find the node where the key is present
+Node* LinkedList::findKey(int key) {
+   Node* ptr = head;
+   while (ptr != NULL && ptr->key != key)
+   {
+      ptr = ptr->next;
+   }
+   return ptr;
+}
+
+void LinkedList::insert(Node* prev, int newKey) {
+  //Check if head is Null i.e list is empty
+   if(head == NULL){
+      head = new Node;
+      head->key = newKey;
+      head->next = NULL;
+   }
+   // if list is not empty, look for prev and append our node there
+   else if(prev == NULL)
+   {
+      Node* newNode = new Node;
+      newNode->key = newKey;
+      newNode->next = head;
+      head = newNode;
+   }
+   else {
+      Node* newNode = new Node;
+      newNode->key = newKey;
+      newNode->next = prev->next;
+      prev->next = newNode;
+   }
+}
+
+// Print the keys in your list
+void LinkedList::displayList() {
+   Node* temp = head;
+
+   if(temp == NULL){
+      return;
+   }
+
+   while(temp->next != NULL){
+       cout<< temp->key <<" -> ";
+      temp = temp->next;
+   }
+
+   cout<<temp->key<<endl;
+}
+
+// TODO
+// Delete node at a particular index
+bool LinkedList::removeAtLocation(int n)
+{
+   bool isDeleted = false;
+
+   if(head == NULL){
+      cout<< "List is empty"<<endl;
+      return isDeleted;
+   }
+
+   // Special case to delete the head
+   if (n == 0) {
+       //todo
+       Node *temp = head;
+       head = head->next;
+       delete temp;
+       temp = nullptr;
+   }
+
+   Node *pres = head;
+   Node *prev = NULL;
+
+   //TODO
+   for (int i = 0; i < n - 1; i++) {
+      if (pres->next == NULL) {
+         cout << "N is larger than the length of the list." << endl;
+         return isDeleted;
+      }
+      pres = pres->next;
+   }
+   prev = pres;
+   if (pres->next == NULL) {
+      cout << "N is larger than the length of the list." << endl;
+      return isDeleted;
+   }
+   pres = pres->next;
+
+   prev->next = pres->next;
+   delete pres;
+   pres = prev->next;
+
+   return isDeleted;
+}
---
+++ readme.txt
@@ -0,0 +1,17 @@
+You are given 3 files: recitation5.cpp, LinkedList.cpp, and LinkedList.hpp
+
+Compile using the following:
+	g++ recitation5.cpp LinkedList.cpp -o recitation5
+
+Complete the following functions:
+
+~LinkedList()
+	Destructor
+removeAtLocation()
+	Given a position in the linked list, delete the node at that position.
+findKey()
+	Find a specific key and return a pointer to that node, else return NULL
+
+The test cases are in the main function (rectitation5.cpp).
+
+Once you're finished, zip the 3 solutions files and attach the zipped file to the Recitation 5 Dropbox on Canvas.
